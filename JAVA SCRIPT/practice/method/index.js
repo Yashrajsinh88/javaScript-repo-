1fb26d3166f030e2a18eec39ecfbaf363984d7e5
Yashrajsinh <yashrajsinhdodiya888@gmail.com>
2024-07-25 11:22:51 +0530

    JavaScript

---
+++ JAVA SCRIPT/practice/method/index.js
@@ -0,0 +1,34 @@
+let arr = [100,200,300,400,500,600,700,800,900];
+console.log(arr);
+ console.log(arr.includes());
+ console.log(arr);
+
+ console.log(arr.length);
+ console.log(arr);
+
+ console.log(arr.push(1000));
+ console.log(arr);
+
+ console.log(arr.pop());
+ console.log(arr);
+
+ console.log(arr.shift());
+ console.log(arr);
+
+ console.log(arr.unshift(10000));
+ console.log(arr);
+
+ console.log(arr.toString());
+//  console.log(arr);
+
+// console.log(arr.reverse());
+// console.log(arr);
+
+console.log(arr.indexOf(600));
+// console.log(arr);
+
+console.log(arr.slice(2,6));
+console.log(arr);
+
+console.log(arr.splice(5,5,2000,3000));
+console.log(arr);

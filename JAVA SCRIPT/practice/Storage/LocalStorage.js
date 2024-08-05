62f3b5bdc22ab652c34f20aa7d82b02c1f23be87
Yashrajsinh <yashrajsinhdodiya888@gmail.com>
2024-08-05 19:29:09 +0530

    JavaScript

---
+++ JAVA SCRIPT/practice/Storage/LocalStorage.js
@@ -0,0 +1,7 @@
+localStorage.setItem("name","Yashrajsinh");
+localStorage.setItem("age","21");
+
+const data1 =  localStorage.getItem("name");
+console.log(data1);
+
+localStorage.removeItem("name");

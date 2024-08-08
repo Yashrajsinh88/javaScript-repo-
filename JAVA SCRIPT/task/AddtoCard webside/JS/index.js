63925b8536dd70f86bba4ca981ced89b268ec274
Yashrajsinh <yashrajsinhdodiya888@gmail.com>
2024-08-08 10:13:08 +0530

    JavaScript

---
+++ JAVA SCRIPT/task/AddtoCard webside/JS/index.js
@@ -0,0 +1,25 @@
+let counter = document.querySelector(".counter");
+let men = document.querySelectorAll(".men");
+let women = document.querySelectorAll(".women")
+
+let count = 1;
+ function clickme() {
+ 
+//
+// console.log("hello");
+counter.style.display="block";
+for (let i = 0; i < 1; i++) {
+    counter.innerText = count;
+
+    count++;
+    console.log(count)
+    }
+}
+
+function menOnly(){
+    women.forEach(element => {
+        women.style.display="none";  
+        console.log(element);
+       });
+    
+}
